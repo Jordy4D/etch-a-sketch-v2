60397a5844f44f2b8ad37dd658f772a1d35aea2e
Jordy4D_LinuxVM <jsadams27@gmail.com>
2024-03-10 23:04:49 -0400

    completes etch-a-sketch v2 with the first bonus task

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-const gridContainer = document.querySelector(".grid")
+let gridContainer = document.querySelector(".grid")
+let pixels;
 
-const test = 12;
-
-function createRow() {
-    let row = document.createElement('div');
-    row.classList.add('row')
-    const gridSquare = document.createElement("div")
-
-    gridSquare.classList.add('gridSquare')
-    gridSquare.style.width = "calc(100% / " + test + ")";
-    gridSquare.style.height = "calc(100% / " + test + ")";
-    gridContainer.appendChild(gridSquare);
-}
-
-function getPrimaryGrid() {
-    for (let i = 1; i <= test; i++) {
+function getPrimaryGrid(pixels = 16) {
+    for (let i = 1; i <= pixels; i++) {
+        for (let j = 1 ; j <= pixels ; j++ ) {
         
-        // createRow();
+            let row = document.createElement('div');
+            let randomColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
 
-        for (let j = 1 ; j <= test ; j++ ) {
+            row.classList.add('row')
+            const gridSquare = document.createElement("div")
         
-            createRow();
+            gridSquare.classList.add('gridSquare')
+            gridSquare.style.width = "calc(100% / " + pixels + ")";
+            gridSquare.style.height = "calc(100% / " + pixels + ")";
+            gridSquare.addEventListener('mouseenter', function () {
+                    // gridSquare.classList.add('gridDraw')
+                    gridSquare.style.backgroundColor = randomColor;
+                });
+            gridContainer.appendChild(gridSquare);
             
         }
     } 
 }
 
-function userGrid(input) {
 
+function userGrid() {
+    pixels = prompt("Enter a number 100 or less for the etch-a-sketch canvas");
+    gridContainer.innerHTML = '';
+
+    while (pixels > 100 || pixels === null || pixels === '') {
+        pixels = prompt("Please choose a number 100 or less");
+    }
+    return getPrimaryGrid(pixels);
 }
 
-function gridDraw() {
-
+function eraseGrid() {
+    gridContainer.innerHTML = '';
+    getPrimaryGrid(pixels);
 }
 
 getPrimaryGrid();
